bf6285dc108855e5a96af8f7c4e56067599ad181
NoorAlaidaros <137597919+NoorAlaidaros@users.noreply.github.com>
2023-06-24 16:11:34 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+  'name': 'Car Store',
+  'version': '1.0',
+  'category': 'Accounting/Accounting',
+  'author': 'Noor Alaidaros',
+  'website': "",
+  'company': 'Noor Alaidaros',
+  'maintainer': 'Noor Alaidaros',
+  'summary': 'Record Car Details',
+  "description": "",
+  'depends': ['base'],
+   'data': [
+      'security/car_store.csv',
+      'views/car_store_views.xml',
+  ],
+  'installable':True,
+  'application':True,
+  'license':'LGPL-3',
+ 
+}
